ffa5eb5986e41b7e95fbf9937be9d3421f29b6e3
francesco palazzini <francesco@default-centos-73.vagrantup.com>
2018-07-26 17:17:36 +0800

    fix(setup): vim directory creation

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/.config/{i3,i3status} && \
   ln -s $(pwd)/i3/config $HOME/.config/i3/config && \
   ln -s $(pwd)/i3status/config $HOME/.config/i3status/config
 
-mkdir -p $HOME/.vim/colors
+mkdir -p $HOME/.vim
 ln -s $(pwd)/vim/vimrc $HOME/.vimrc
 ln -s $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
 ln -s $(pwd)/vim/colors/ $HOME/.vim/colors
