478497c754d56a102e735a73ae1c2cb136728e9a
francesco palazzini <francesco@default-centos-73.vagrantup.com>
2018-07-27 13:06:11 +0800

    ref(setup): improved link creation

--- setup.sh
+++ setup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 mkdir -p $HOME/.config/{i3,i3status} && \
-  ln -s $(pwd)/i3/config $HOME/.config/i3/config && \
-  ln -s $(pwd)/i3status/config $HOME/.config/i3status/config
+  ln -sf $(pwd)/i3/config $HOME/.config/i3/config && \
+  ln -sf $(pwd)/i3status/config $HOME/.config/i3status/config
 
-mkdir -p $HOME/.vim
-ln -s $(pwd)/vim/vimrc $HOME/.vimrc
-ln -s $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
-ln -s $(pwd)/vim/colors/ $HOME/.vim/colors
+mkdir -p $HOME/.vim/colors && \
+  ln -sf $(pwd)/vim/vimrc $HOME/.vimrc && \
+  ln -sf $(pwd)/tmux/tmux.conf $HOME/.tmux.conf
+
+# suppress error code and error message
+cp $(pwd)/vim/colors/* $HOME/.vim/colors/ 2>/dev/null || :
